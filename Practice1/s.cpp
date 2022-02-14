d7e265366d03f87a5040c2894b0ed2e3664b518b
李宵逸 <13691419520@163.com>
2022-02-14 13:30:02 -0500

    init

---
+++ Practice1/s.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <cassert>
+// #include <algorithm>
+// #include <vector>
+// #include <set>
+// #include <map>
+// #include <unordered_map>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int T;
+	cin >> T;
+	for (int test = 0; test < T; test++) {
+		int ans = 0;
+		cout << "Case #" << (test + 1) << ": " << (ans) << endl;
+	}
+	return 0;
+}
+
---
+++ Practice1/s.py
@@ -0,0 +1,22 @@
+try:
+	import os, sys
+	stdin = sys.stdin
+	if len(sys.argv) > 1:
+		stdin = open(sys.argv[1])
+	else:
+		# stdin = open('s.txt')
+		stdin = open(os.path.splitext(__file__)[0] + '.txt')
+	input = lambda: stdin.readline()[:-1]
+except Exception:
+	pass
+
+# import math, sys
+# sys.setrecursionlimit(100000000)
+# from collections import defaultdict
+# A = list(map(int, input().split()))
+
+T = int(input())
+for test in range(T):
+
+	print('Case #%d:' % (test + 1), ans)
+
